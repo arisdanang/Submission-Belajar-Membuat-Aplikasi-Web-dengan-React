5f2513aa071799c7974fdf354838a76f4602994e
Aris Danang Pridiksono <arisdanang2810@gmail.com>
2022-06-14 11:42:54 +0700

    fixing size input navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const Navbar = ({ onSearchHandler, query }) => {
 	return (
 		<div className="note-app__header flex justify-between items-center px-3 py-3 max-w-7xl mx-auto border-b-2">
-			<h1 className="text-2xl text-white font-bold mr-2">Notes</h1>
+			<h1 className="text-2xl text-white font-bold mr-9">Notes</h1>
 			<div className="note-search">
 				<input
 					type="text"
 					placeholder="Search..."
-					className="py-2 px-3 rounded-md bg-transparent border-dark-secondary border-2 text-white w-full"
+					className="py-2 px-4 rounded-md bg-transparent border-dark-secondary border-2 text-white w-full"
 					value={query}
 					onChange={onSearchHandler}
 				/>
---
+++ src/context/NoteTypes.js
@@ -0,0 +1,6 @@
+/**
+ * @description   Types is just for constant that for dispatching our action in order to reducing a Misspelling
+ */
+
+export const CREATE_NOTE = "CREATE_NOTE";
+export const DELETE_NOTE = "DELETE_NOTE";
