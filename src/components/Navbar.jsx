92d76656f0ebf62195e06fbdca305d614871ef6f
Aris Danang Pridiksono <arisdanang2810@gmail.com>
2022-06-14 11:45:02 +0700

    add margin-right

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = ({ onSearchHandler, query }) => {
 	return (
 		<div className="note-app__header flex justify-between items-center px-3 py-3 max-w-7xl mx-auto border-b-2">
-			<h1 className="text-2xl text-white font-bold mr-9">Notes</h1>
+			<h1 className="text-2xl text-white font-bold mr-12">Notes</h1>
 			<div className="note-search">
 				<input
 					type="text"
